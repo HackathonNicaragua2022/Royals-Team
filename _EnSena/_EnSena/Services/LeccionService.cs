80b948fb7139c1920f0417ddf911836bef70cef3
GabrielRdz15 <rod87riguez15@gmail.com>
2022-11-25 20:44:40 -0600

    lecciones y roles con servicios basicos de crud completados

---
+++ _EnSena/_EnSena/Services/LeccionService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Firebase.Database.Query;
+using _EnSena.Models;
+using Firebase.Database;
+using System.Threading.Tasks;
+
+namespace _EnSena.Services
+{
+    public class LeccionService
+    {
+        FirebaseClient client;
+        public LeccionService()
+        {
+            client = new FirebaseClient("alguna mamada de enlace");
+
+        }
+
+
+
+        public async Task<bool> isLeccionExist(Leccion toComprove)
+        {
+            var cuestionarioExist = (await client.Child("Lecciones").OnceAsync<Leccion>()).
+                       Where(u => u.Object == toComprove).FirstOrDefault();
+
+            return (toComprove != null);
+        }
+
+
+        public async Task<bool> RegistarNuevaLeccion(Leccion newObj)
+        {
+            if (await isLeccionExist(newObj) == false)
+            {
+                await client.Child("Lecciones").PostAsync(newObj);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<bool> UpdateLeccion(Leccion toUpdate)
+        {
+            var toupdateObj = (await client.Child("Lecciones").OnceAsync<Leccion>())
+                .FirstOrDefault(u => u.Object == toUpdate);
+
+            await client.Child("Lecciones")
+                .Child(toupdateObj.Key)
+                .PutAsync(toUpdate);
+            return true;
+        }
+
+        public async Task<bool> DeleteLeccion(Leccion toDelete)
+        {
+            var todeleteObj = (await client.Child("Lecciones").OnceAsync<Leccion>())
+                .FirstOrDefault(u => u.Object == toDelete);
+
+            await client.Child("Curses")
+                .Child(todeleteObj.Key)
+                .DeleteAsync();
+            return true;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ _EnSena/_EnSena/Services/RolUsuarioService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Firebase.Database.Query;
+using _EnSena.Models;
+using Firebase.Database;
+using System.Threading.Tasks;
+
+namespace _EnSena.Services
+{
+    class RolUsuarioService
+    {
+        FirebaseClient client;
+        public RolUsuarioService()
+        {
+            client = new FirebaseClient("alguna mamada de enlace");
+
+        }
+
+
+
+        public async Task<bool> isRolExist(RolUsuario toComprove)
+        {
+            var cuestionarioExist = (await client.Child("Roles").OnceAsync<RolUsuario>()).
+                       Where(u => u.Object == toComprove).FirstOrDefault();
+
+            return (toComprove != null);
+        }
+
+
+        public async Task<bool> RegistrarNuevoRol(RolUsuario newObj)
+        {
+            if (await isRolExist(newObj) == false)
+            {
+                await client.Child("Roles").PostAsync(newObj);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public async Task<bool> UpdateRol(RolUsuario toUpdate)
+        {
+            var toupdateObj = (await client.Child("Roles").OnceAsync<RolUsuario>())
+                .FirstOrDefault(u => u.Object == toUpdate);
+
+            await client.Child("Roles")
+                .Child(toupdateObj.Key)
+                .PutAsync(toUpdate);
+            return true;
+        }
+
+        public async Task<bool> DeleteRol(RolUsuario toDelete)
+        {
+            var todeleteObj = (await client.Child("Roles").OnceAsync<RolUsuario>())
+                .FirstOrDefault(u => u.Object == toDelete);
+
+            await client.Child("Roles")
+                .Child(todeleteObj.Key)
+                .DeleteAsync();
+            return true;
+        }
+
+
+
+    }
+}
